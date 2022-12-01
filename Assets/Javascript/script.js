711efb0ab26b25caf9a14ae686cbf140434f18af
Eliu Rosario <eliurosario@gmail.com>
2022-12-01 00:43:08 -0500

    Added style to footer link. Initiated Java code

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,1 +1,40 @@
-//Java File//
+//Questions//
+var questionnaire = [
+    {
+    quizquestion:"Javascript is known as...",
+    questionoptions: ["Coffee", "Best Language ever!", "Latest Programming Language"
+, "Programming language of the web"],
+    correctanswer: "Programming language of the web" 
+},
+
+{
+    quizquestion:"How does Java represents TEXTS?",
+    questionoptions: ["Boolean", "Variable", "Array", "String"],
+    correctanswer: "String" 
+},
+
+{
+    quizquestion:"",
+    questionoptions: [],
+    correctanswer: "" 
+},
+
+{
+    quizquestion:"",
+    questionoptions: [],
+    correctanswer: "" 
+},
+
+{
+    quizquestion:"",
+    questionoptions: [],
+    correctanswer: "" 
+},
+
+{
+    quizquestion:"",
+    questionoptions: [],
+    correctanswer: "" 
+},
+]
+
